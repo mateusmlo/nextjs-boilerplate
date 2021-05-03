9239cd289967be88df3fac917b8a21de60c2529a
Mateus Oliveira <mateus.mlo95@gmail.com>
2021-05-03 17:47:40 -0300

    next js boilerplate complete

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+/* Basic.args = {
+  title: 'React Avançado',
+  description: 'Descrição'
+} */
